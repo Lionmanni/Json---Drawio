5bee2c3ab268d850973fadd238e27458a16e2a46
Lionmanni <148263047+Lionmanni@users.noreply.github.com>
2023-11-16 17:25:14 +0100

    Delete graphviz2drawio/Old_files directory

--- graphviz2drawio/Old_files/DOT-py-testfile.py
+++
@@ -1,8 +0,0 @@
-
-Connections = ['A - B', 'A - C']
-
-Entities = {"A": {"group": "1"},
-            "B": {"group": "1", "image": r"/home/andre/Downloads/Albis_JSON->DRAWIO/Albis_Vizual_Project/Image_Test_files/tree-smaller2.jpg"},
-            "C": {"group": "1"}}
-            
-

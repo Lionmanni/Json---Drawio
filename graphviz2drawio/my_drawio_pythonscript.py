4929d3fd8a689c4dcfa1bb649ef457a76b46abfb
Lionmanni <148263047+Lionmanni@users.noreply.github.com>
2023-11-17 16:56:56 +0100

    Delete graphviz2drawio/my_drawio_pythonscript.py

--- graphviz2drawio/my_drawio_pythonscript.py
+++
@@ -1,5 +0,0 @@
-from graphviz2drawio import graphviz2drawio
-
-graph_to_convert = r'/home/andre/Downloads/Albis_JSON->DRAWIO/Albis_Vizual_Project/Python_scripts/DOT-py-testfile.dot'
-xml = graphviz2drawio.convert(graph_to_convert)
-print(xml)
